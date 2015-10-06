e982d82b33d51678a138479a0ac2e23edb37d3ae
Steven Wooding <contact@stevenwooding.com>
2015-10-06 16:45:24 +0100

    feat: Initial commit of database schema

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,19 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+-- Delete any old tournament database that might be hanging around
+DROP DATABASE IF EXISTS tournament;
 
+-- Create the tournament database
+CREATE DATABASE tournament;
+
+-- Connect to the tournament database
+\c tournament;
+
+-- Table containing the registered players, id and name.
+CREATE TABLE players ( id serial PRIMARY KEY,
+                       name text);
+
+-- Table of matchers played, each row having the winner and loser ids.
+CREATE TABLE matches ( winner_pid int references players(id),
+                       loser_pid int references players(id));
