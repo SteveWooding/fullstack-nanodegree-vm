458a483d1216c0205cecea89818083b02be95742
Steven Wooding <contact@stevenwooding.com>
2015-10-27 15:04:33 +0000

    feat: Display menus of restaurants via the URL

--- vagrant/menu/project.py
+++ vagrant/menu/project.py
@@ -4,6 +4,7 @@ app = Flask(__name__)
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
+from sqlalchemy.orm.exc import NoResultFound
 
 engine = create_engine('sqlite:///restaurantmenu.db')
 Base.metadata.bind = engine
@@ -12,9 +13,13 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 @app.route('/')
-@app.route('/hello')
-def HelloWorld():
-    restaurant = session.query(Restaurant).first()
+@app.route('/restaurants/<int:restaurant_id>/')
+def restaurant_menu(restaurant_id):
+    try:
+        restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    except NoResultFound:
+        return "No restaurant exists with that ID."
+
     items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
     output = ''
     for item in items:
