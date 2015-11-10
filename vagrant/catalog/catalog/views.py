e9cb41281048fd868151c42d18dc49c22c5383ba
Steven Wooding <contact@stevenwooding.com>
2015-11-10 10:42:43 +0000

    feat: Render the items.html page + fix URLs
    Forgot to end each target URL in a forward slash.

--- vagrant/catalog/catalog/views.py
+++ vagrant/catalog/catalog/views.py
@@ -42,26 +42,29 @@ def show_homepage():
                            latest_items=items)
 
 
-@app.route('/catalog/<category_name>/items')
+@app.route('/catalog/<category_name>/items/')
 def show_items(category_name):
     """Show items belonging to a specified category."""
-    return "Items belonging to the %s category." % category_name
+    return render_template('items.html',
+                           categories=categories,
+                           category=category,
+                           items=items)
 
 
-@app.route('/catalog/<category_name>/<item_name>')
+@app.route('/catalog/<category_name>/<item_name>/')
 def show_item(category_name, item_name):
     """Show details of a particular item belonging to a specified category."""
     return ("Show details of item %s in category %s."
             % (item_name, category_name))
 
 
-@app.route('/catalog/<item_name>/edit')
+@app.route('/catalog/<item_name>/edit/')
 def edit_item(item_name):
     """Edit the details of the specified item."""
     return "Edit details of the %s item." % item_name
 
 
-@app.route('/catalog/<item_name>/delete')
+@app.route('/catalog/<item_name>/delete/')
 def delete_item(item_name):
     """Delete a specified item from the database."""
     return "Delete the %s item." % item_name
