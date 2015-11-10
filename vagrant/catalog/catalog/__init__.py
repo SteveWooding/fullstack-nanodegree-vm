76510f560bab61b7e305418e31a1b0fdc759a75d
Steven Wooding <contact@stevenwooding.com>
2015-11-10 14:53:51 +0000

    feat: Connect the catlog package to the database

--- vagrant/catalog/catalog/__init__.py
+++ vagrant/catalog/catalog/__init__.py
@@ -1,5 +1,15 @@
 """Initialisation for the catalog package."""
 from flask import Flask
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from database_setup import Base
+
 app = Flask(__name__)
+
+engine = create_engine('sqlite:///itemcatalog.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 import catalog.views
