d2878f3f3072c72a31117fd9a52feacf42b2c8b8
Steven Wooding <contact@stevenwooding.com>
2015-11-11 16:05:30 +0000

    feat: Update items_json() to output real JSON

--- vagrant/catalog/catalog/json.py
+++ vagrant/catalog/catalog/json.py
@@ -2,8 +2,16 @@
 from flask import jsonify
 
 from catalog import app, session
+from database_setup import Category
 
 @app.route('/catalog.json/')
 def items_json():
-    """Returns all the items in the catalog as a JSON file."""
-    return "Test"
+    """Returns all the items in the catalog as a JSON file.
+
+    The for loop in the call to jsonify() goes through each category and,
+    because the Category class has a reference to the items in it, for each
+    item a call to its serialise function is made. So we end up with a JSON
+    array of items for each category.
+    """
+    categories = session.query(Category).all()
+    return jsonify(Category=[i.serialise for i in categories])
