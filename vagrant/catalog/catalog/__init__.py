f575deba1a6b71e0d4f4fb463dffde70b5964176
Steven Wooding <contact@stevenwooding.com>
2015-11-12 15:29:16 +0000

    feat: Add configuration for image uploads

--- vagrant/catalog/catalog/__init__.py
+++ vagrant/catalog/catalog/__init__.py
@@ -5,8 +5,14 @@ from sqlalchemy.orm import sessionmaker
 
 from database_setup import Base
 
-app = Flask(__name__)
+UPLOAD_FOLDER = '/vagrant/catalog/item_images'
+ALLOWED_EXTENSIONS = set(['jpg', 'jpeg', 'png', 'gif'])
 
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 4 * 1024 * 1024  # 4MB
+
+# Connect to the database
 engine = create_engine('sqlite:///itemcatalog.db')
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
