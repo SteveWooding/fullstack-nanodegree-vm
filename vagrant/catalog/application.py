8593dcf39dd36837e6bae2ae822a5ab684c07515
Steven Wooding <contact@stevenwooding.com>
2015-12-17 11:39:22 +0000

    feat: Add support for PostgreSQL database

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -12,13 +12,18 @@ from catalog.populate_database import populate_database
 
 if __name__ == '__main__':
     # App configuration
-    app.config['DATABASE_URL'] = 'sqlite:///itemcatalog.db'
+    #app.config['DATABASE_URL'] = 'sqlite:///itemcatalog.db'
+    app.config['DATABASE_URL'] = 'postgresql://catalog:PASSWORD@localhost/catalog'
     app.config['UPLOAD_FOLDER'] = '/vagrant/catalog/item_images'
     app.config['ALLOWED_EXTENSIONS'] = set(['jpg', 'jpeg', 'png', 'gif'])
     app.config['MAX_CONTENT_LENGTH'] = 4 * 1024 * 1024  # 4 MB
     app.secret_key = 'super_secret_key'  # This needs changing in production env
 
-    if os.path.isfile('itemcatalog.db') is False:
+    if app.config['DATABASE_URL'] == 'sqlite:///itemcatalog.db':
+        if os.path.isfile('itemcatalog.db') is False:
+            create_db(app.config['DATABASE_URL'])
+            populate_database()
+    else:  # for postgresql
         create_db(app.config['DATABASE_URL'])
         populate_database()
 
